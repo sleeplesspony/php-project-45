b7bd2c5c3c3fa672d2907804cd0a253c87fef818
Natalia Kozlova <sleeplesspony@gmail.com>
2025-05-21 11:57:09 +0300

    fix code format issues

--- src/Cli.php
+++ src/Cli.php
@@ -5,8 +5,8 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greetUser() {
-        
+function greetUser()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
