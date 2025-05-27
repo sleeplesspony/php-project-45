a0774102b158459cf53478c74d9c814313a79a1b
Natalia Kozlova <sleeplesspony@gmail.com>
2025-05-27 09:05:52 +0300

    refactor Even game logic to use Engine.php

---
+++ src/Games/Even.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use BrainGames\Engine;
+
+function startGame()
+{
+    $game = createGame();
+    Engine\start($game);
+}
+
+function createGame()
+{
+
+    $game = [
+        "task" => 'Answer "yes" if the number is even, otherwise answer "no".',
+        "rounds" => getRounds()
+    ];
+
+    return $game;
+}
+
+function getRounds()
+{
+    $rounds = [];
+
+    for ($i = 1; $i <= Engine\MAX_QUESTIONS_NUM; $i++) {
+        $num = rand(1, Engine\MAX_RAND);
+
+        $rounds[] = [
+            "question" => "{$num}",
+            "answer" => strval(getAnswer($num))
+        ];
+    }
+
+    return $rounds;
+}
+
+function getAnswer($num)
+{
+    return isEven($num) ? "yes" : "no";
+}
+
+function isEven($num)
+{
+    return $num % 2 === 0;
+}
